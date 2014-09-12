7a4cf2aee498da38e5cf2ce77a6c84d386559f5f
March Liu <march.liu@gmail.com>
2014-09-12 23:55:36 +0800

    ext type set test pass

--- type_test.go
+++ type_test.go
@@ -16,4 +16,13 @@ func TestTypeFound(t *testing.T) {
 	if err != nil {
 		t.Fatalf("except define a money var but error: %v", err)
 	}
+	g.Setvar("bill", m)
+
+	mny, ok := g.Lookup("bill")
+	if !ok {
+		t.Fatalf("money var bill as %v not found ", m)
+	}
+	if !reflect.DeepEqual(m, mny) {
+		t.Fatalf("except got money var bill as %v but %v", m, mny)
+	}
 }
