36ce0b1902f7a0c1bbac6d4b4fb2c826ac1d4f3f
March Liu <march.liu@gmail.com>
2014-09-25 10:59:24 +0800

    rename parsec.go to parses.go

---
+++ linq.go
@@ -0,0 +1,41 @@
+package gisp
+
+type T interface{}
+
+type Linq interface {
+	From(input T) Linq
+	Range(start, count int)
+	All(f func(T) (bool, error)) (all bool, err error)
+	Any() (exists bool, err error)
+	AnyWith(f func(T) (bool, error)) (exists bool, err error)
+	Average() (avg float64, err error)
+	Count() (count int, err error)
+	CountBy(f func(T) (bool, error)) (c int, err error)
+	Distinct() Linq
+	DistinctBy(f func(T, T) (bool, error)) Linq
+	ElementAt(i int) (elem T, found bool, err error)
+	Except(inputSlice T) Linq
+	First() (elem T, found bool, err error)
+	FirstBy(f func(T) (bool, error)) (elem T, found bool, err error)
+	GroupBy(keySelector func(T) T, valueSelector func(T) T) (map[T][]T, error)
+	GroupJoin(innerSlice T, outerKeySelector func(T) T, innerKeySelector func(T) T, resultSelector func(outer T, inners []T) T) Linq
+	Intersect(inputSlice T) Linq
+	Join(innerSlice T, outerKeySelector func(T) T, innerKeySelector func(T) T, resultSelector func(outer T, inner T) T) Linq
+	Last() (elem T, found bool, err error)
+	LastBy(f func(T) (bool, error)) (elem T, found bool, err error)
+	Max() (max T, err error)
+	Min() (min T, err error)
+	OrderBy(less func(this T, that T) bool) Linq
+	Order(func(x, y T) (bool, error)) Linq
+	Results() (List, error)
+	Reverse() Linq
+	Select(f func(T) (T, error)) Linq
+	Single(f func(T) (bool, error)) (single T, err error)
+	Skip(n int) Linq
+	SkipWhile(f func(T) (bool, error)) Linq
+	Sum() (sum float64, err error)
+	Take(n int) Linq
+	TakeWhile(f func(T) (bool, error)) Linq
+	Union(inputSlice T) Linq
+	Where(f func(T) (bool, error)) Linq
+}
